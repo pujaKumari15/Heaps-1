b8a13efdf4e85af1226ed9a05c53eac87a434ede
pujakumari <puja.kumari@sjsu.edu>
2024-07-19 16:41:52 -0700

    Added code

--- KthLargest.java
+++ KthLargest.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class KthLargest {
+import java.util.PriorityQueue;
+
+/***
+ TC - O(nlog(n-k))
+ SC - O(n-k)
+ */
+class KthLargest {
+    public int findKthLargest(int[] nums, int k) {
+        int n = nums.length;
+        int min = Integer.MAX_VALUE;
+
+        //Using max heap
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b-a);
+
+        for(int num : nums) {
+            pq.add(num);
+            if(pq.size() > (n-k))
+                min = Math.min(min, pq.poll());
+        }
+
+        return min;
+
+    }
 }
--- MergeKSortedLists.java
+++ MergeKSortedLists.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class MergeKSortedLists {
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+/***
+ Using min heap
+ TC - O(nklogk)
+ SC - O(k)
+ */
+class MergeKSortedLists {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists == null || lists.length ==0)
+            return null;
+
+        ListNode dummy = new ListNode(-1);
+        ListNode curr = dummy;
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((a,b) -> (a.val - b.val));
+
+        for(ListNode node : lists) {
+            if(node != null)
+                pq.add(node);
+        }
+
+        while(!pq.isEmpty()) {
+            ListNode min = pq.poll();
+            curr.next = min;
+            curr = curr.next;
+
+            if(min.next != null){
+                pq.add(min.next);
+            }
+        }
+
+        return dummy.next;
+
+    }
 }
